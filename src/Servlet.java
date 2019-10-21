d3977a3dd6c04757b605de1d09b175dbdb68fe8a
Tanuyoki <tanuyoki0@gmail.com>
2019-10-21 23:37:47 +0800

    commit

--- src/Servlet.java
+++ src/Servlet.java
@@ -37,10 +37,20 @@ public class Servlet extends HttpServlet {
             JDBCUtils jdbcUtils = new JDBCUtils();
             Connection connection = jdbcUtils.getConnection();
             Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM all_user WHERE user_id " +
+                    "IN (SELECT MAX(user_id) FROM all_user)");
+            int abc = 0;
+            while (resultSet.next()) {
+                abc = resultSet.getInt(1);
+                System.out.println(abc);
+            }
+
             boolean isSuccess = statement.execute("INSERT INTO all_user " +
-                    "VALUES (10001, 'Test', '123456', '15200001111', 'HelloWorld', '1000-00-00', 1, '广东省', 1, 1, 1, 1)");
+                    "VALUES (0, 'Test', '123456', '15200001111', 'HelloWorld', '1000-00-00', 1, '广东省', 1, 1, 1, 1)");
             System.out.println(isSuccess);
 
+            jdbcUtils.close();
 //            String resultData = parseData(jsonObject);
 //            out.print(resultData);
 //            out.flush();
